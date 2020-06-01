d0a895307dede072245c13aea887587d4271fd44
Ashutosh Shukla <ashutoshshukla004@gmail.com>
2020-06-01 16:31:25 +0530

    add compress combine css

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express=require('express');
 const request=require('request');
+const compression = require('compression')
 const app=express();
 app.set('view engine','ejs');
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(compression()); //use compression 
+app.use(express.static("public"));
 
 app.get('/',function(req,res){
     var url='https://api.covid19india.org/data.json';
@@ -30,6 +31,6 @@ app.get('/state/:state',function(req,res){
     });
 });
 
-app.listen(process.env.PORT,process.env.IP,function(){
+app.listen(8080,process.env.IP,function(){
     console.log("Tracker Server online!");
 });
