021d101f8090438b7eaa4c78691aa5f9e20f315d
Ashutosh Shukla <ashutoshshukla004@gmail.com>
2020-06-01 16:19:42 +0530

    compress and combine css

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express=require('express');
 const request=require('request');
 const app=express();
 app.set('view engine','ejs');
-app.use(express.static("public"));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/',function(req,res){
     var url='https://api.covid19india.org/data.json';
